67bd4cd5298e25cab4b46948f0d58fa0c72df6fa
lumyslinski <luk@mysl.tech>
2018-06-23 01:51:15 +0200

    Added singleton

---
+++ python/creational/singleton.py
@@ -0,0 +1,21 @@
+class Singleton:
+    __instance = None
+
+    class __Singleton:
+        def getUniqueId(self):
+            return str(self) + ',id='+str(id(self))
+
+    def __new__(self):
+        if Singleton.__instance is None:
+             Singleton.__instance = Singleton.__Singleton()
+        return Singleton.__instance
+
+s = Singleton() 
+print(s)
+
+s1 = Singleton()
+print(s1.getUniqueId())
+
+s2 = Singleton()
+print(s2.getUniqueId())
+
